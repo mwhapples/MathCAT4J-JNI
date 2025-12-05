2b224952c8c38c42d3be02d00eb9f14725783ed3
Michael Whapples <software@mdw.onl>
2025-12-05 18:49:29 +0000

    Move to jna-jpms to support modules

--- mathcat4j-jni/src/test/java/onl/mdw/mathcat4j/jni/TestLibraryLoader.java
+++ mathcat4j-jni/src/test/java/onl/mdw/mathcat4j/jni/TestLibraryLoader.java
@@ -7,14 +7,18 @@
  */
 package onl.mdw.mathcat4j.jni;
 
-import io.questdb.jar.jni.JarJniLoader;
+import com.sun.jna.Native;
+import com.sun.jna.Platform;
 import onl.mdw.mathcat4j.jni.libs.LibInfo;
+
+import java.io.File;
 
 public class TestLibraryLoader implements LibraryLoader {
     @Override
     public boolean load() {
         try {
-            JarJniLoader.loadLib(LibInfo.class, LibInfo.PATH_PREFIX, LibInfo.LIB_NAME);
+            final File libFile = Native.extractFromResourcePath(String.format("%s/%s/%s", LibInfo.PATH_PREFIX, Platform.RESOURCE_PREFIX, System.mapLibraryName(LibInfo.LIB_NAME)));
+            System.load(libFile.getAbsolutePath());
             return true;
         } catch (Exception e) {
             return false;
